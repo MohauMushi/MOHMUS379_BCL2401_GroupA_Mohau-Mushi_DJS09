ba7c5382a84c1b17d53fe2f296b04984319c553b
Mohau Mushi <david1mushi1@gmail.com>
2024-06-07 16:19:53 +0200

    moved  showReviewTotal, populateUser into their own utils, and also moved querySelectors

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,64 @@
-const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
-const userNameDisplay = document.querySelector('#user') as HTMLElement
-const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
+import { showReviewTotal, populateUser } from "./utils";
+let isOpen: boolean;
 
-let isOpen : boolean
-
-const reviews : {
-    name: string;
-    stars: number;
-    loyaltyUser: boolean;
-    date: string;
+// Reviews
+const reviews: {
+  name: string;
+  stars: number;
+  loyaltyUser: boolean;
+  date: string;
 }[] = [
-    {
-        name: 'Sheia',
-        stars: 5,
-        loyaltyUser: true,
-        date: '01-04-2021'
-    },
-    {
-        name: 'Andrzej',
-        stars: 3,
-        loyaltyUser: false,
-        date: '28-03-2021'
-    },
-    {
-        name: 'Omar',
-        stars: 4,
-        loyaltyUser: true,
-        date: '27-03-2021'
-    },
-]
+  {
+    name: "Sheia",
+    stars: 5,
+    loyaltyUser: true,
+    date: "01-04-2021",
+  },
+  {
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: false,
+    date: "28-03-2021",
+  },
+  {
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: true,
+    date: "27-03-2021",
+  },
+];
 
-function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
-    const iconDisplay = isLoyalty ? '⭐' : ''
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
-}
-
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
-
+// User
 const you: {
-    firstName : string;
-    lastName: string;
-    isReturning: boolean;
-    age: number;
-    stayedAt : string[]; 
-
+  firstName: string;
+  lastName: string;
+  isReturning: boolean;
+  age: number;
+  stayedAt: string[];
 } = {
-   firstName: 'Bobby',
-   lastName: 'Brown',
-   isReturning: true,
-   age: 35,
-   stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
-}
+  firstName: "Bobby",
+  lastName: "Brown",
+  isReturning: true,
+  age: 35,
+  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
+};
 
-console.log(you.firstName)
+//Properties
+const properties: {
+  image: string;
+  title: string;
+  price: number;
+  location: {
+    firstLine: string;
+    city: string;
+    code: number;
+    country: string;
+  };
+  contact: string;
+  isAvailable: boolean;
+}[] = [{}];
 
-function populateUser(isReturning : boolean, userName : string ) {
-    if (isReturning){
-        returningUserDisplay.innerHTML = 'back'
-    }
-    userNameDisplay.innerHTML = userName
-}
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
 
-populateUser(you.isReturning, you.firstName)
+populateUser(you.isReturning, you.firstName);
