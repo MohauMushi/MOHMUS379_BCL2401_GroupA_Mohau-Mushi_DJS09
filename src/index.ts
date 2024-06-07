7618264801c635e2deed60d53f9d976d35a96a43
Mohau Mushi <david1mushi1@gmail.com>
2024-06-07 15:37:30 +0200

    Object challenge for you const

--- src/index.ts
+++ src/index.ts
@@ -32,11 +32,19 @@ function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean)
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
-const you = {
-    userName: 'Bobby',
-    isReturning: true,
+const you: {
+    firstName : string;
+    lastName: string;
+    isReturning: boolean;
+    age: number;
+} = {
+   firstName: 'Bobby',
+   lastName: 'Brown',
+   isReturning: true,
+   age: 35
 }
 
+console.log(you.firstName)
 
 function populateUser(isReturning : boolean, userName : string ) {
     if (isReturning){
@@ -45,4 +53,4 @@ function populateUser(isReturning : boolean, userName : string ) {
     userNameDisplay.innerHTML = userName
 }
 
-populateUser(you.isReturning, you.userName)
+populateUser(you.isReturning, you.firstName)
