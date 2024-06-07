3019c5f60d1107c77057767b0348bcc7b0230f6c
Mohau Mushi <david1mushi1@gmail.com>
2024-06-07 20:16:23 +0200

    enum types

--- src/index.ts
+++ src/index.ts
@@ -31,20 +31,24 @@ const reviews: {
   },
 ];
 
-// User
-const you: {
-  firstName: string;
-  lastName: string;
-  isReturning: boolean;
-  age: number;
-  stayedAt: string[];
-} = {
-  firstName: "Bobby",
-  lastName: "Brown",
-  isReturning: true,
-  age: 35,
-  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
-};
+const ADMIN = 'admin'
+const READ_ONLY = 'read-only'
+
+
+enum Permissions {
+    ADMIN,
+    READ_ONLY
+}
+
+const you = {
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    permissions: Permissions.ADMIN,
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+}
+
 
 //Properties
 const properties: {
