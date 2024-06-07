fa54413e3b6967dc18602fe849b70a03e5ccf853
Mohau Mushi <david1mushi1@gmail.com>
2024-06-07 22:29:47 +0200

    Code Indentation

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,50 @@
-import { showReviewTotal, populateUser, showDetails, getTopTwoReviews } from "./utils";
-import { Permissions, LoyaltyUser } from './enums';
-import { Review, Property } from './interfaces'
+import {
+  showReviewTotal,
+  populateUser,
+  showDetails,
+  getTopTwoReviews,
+} from "./utils";
+import { Permissions, LoyaltyUser } from "./enums";
+import { Review, Property } from "./interfaces";
 import MainProperty from "./classes";
 const propertyContainer = document.querySelector(".properties") as HTMLElement;
-const reviewContainer = document.querySelector('.reviews') as HTMLElement
-const container = document.querySelector('.container') as HTMLElement
-const button = document.querySelector('button') as HTMLElement
-const footer = document.querySelector('.footer') as HTMLElement;
+const reviewContainer = document.querySelector(".reviews") as HTMLElement;
+const container = document.querySelector(".container") as HTMLElement;
+const button = document.querySelector("button") as HTMLElement;
+const footer = document.querySelector(".footer") as HTMLElement;
 
 let IsLoggedIn: boolean;
 
 // Reviews
 const reviews: Review[] = [
-    {
-        name: 'Sheia',
-        stars: 5,
-        loyaltyUser: LoyaltyUser.GOLD_USER,
-        date: '01-04-2021'
-    },
-    {
-        name: 'Andrzej',
-        stars: 3,
-        loyaltyUser: LoyaltyUser.BRONZE_USER,
-        date: '28-03-2021'
-    },
-    {
-        name: 'Omar',
-        stars: 4,
-        loyaltyUser: LoyaltyUser.SILVER_USER,
-        date: '27-03-2021',
-    },
-]
+  {
+    name: "Sheia",
+    stars: 5,
+    loyaltyUser: LoyaltyUser.GOLD_USER,
+    date: "01-04-2021",
+  },
+  {
+    name: "Andrzej",
+    stars: 3,
+    loyaltyUser: LoyaltyUser.BRONZE_USER,
+    date: "28-03-2021",
+  },
+  {
+    name: "Omar",
+    stars: 4,
+    loyaltyUser: LoyaltyUser.SILVER_USER,
+    date: "27-03-2021",
+  },
+];
 
 const you = {
-    firstName: 'Bobby',
-    lastName: 'Brown',
-    permissions: Permissions.ADMIN,
-    isReturning: true,
-    age: 35,
-    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
-}
-
-
+  firstName: "Bobby",
+  lastName: "Brown",
+  permissions: Permissions.ADMIN,
+  isReturning: true,
+  age: 35,
+  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
+};
 
 //Properties
 const properties: Property[] = [
@@ -78,79 +81,84 @@ const properties: Property[] = [
     location: {
       firstLine: "flat 15",
       city: "London",
-      code: 'SW4 5XW',
+      code: "SW4 5XW",
       country: "United Kingdom",
     },
     contact: [+1123495082908, "andyluger@aol.com"],
     isAvailable: true,
   },
   {
-    image: '../src/images/malaysian-hotel.jpeg',
-    title: 'Malia Hotel',
+    image: "../src/images/malaysian-hotel.jpeg",
+    title: "Malia Hotel",
     price: 35,
     location: {
-        firstLine: 'Room 4',
-        city: 'Malia',
-        code: 45334,
-        country: 'Malaysia'
+      firstLine: "Room 4",
+      city: "Malia",
+      code: 45334,
+      country: "Malaysia",
     },
-    contact: [ +60349822083, 'lee34@gmail.com'],
-    isAvailable: false
-}
+    contact: [+60349822083, "lee34@gmail.com"],
+    isAvailable: false,
+  },
 ];
 
 // Functions
 // Functions
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
-populateUser(you.isReturning, you.firstName)
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+populateUser(you.isReturning, you.firstName);
 
 // Add the properties
 for (let i = 0; i < properties.length; i++) {
-    const card = document.createElement('div')
-    card.classList.add('card')
-    card.innerHTML = properties[i].title
-    const image = document.createElement('img')
-    image.setAttribute('src', properties[i].image)
-    card.appendChild(image)
-    showDetails(you.permissions, card, properties[i].price)
-    propertyContainer.appendChild(card)
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = properties[i].title;
+  const image = document.createElement("img");
+  image.setAttribute("src", properties[i].image);
+  card.appendChild(image);
+  showDetails(you.permissions, card, properties[i].price);
+  propertyContainer.appendChild(card);
 }
 
-
-let count = 0
-function addReviews(array: Review[]) : void {
-    if (!count ) {
-        count++
-        const topTwo = getTopTwoReviews(array)
-        for (let i = 0; i < topTwo.length; i++) {
-            const card = document.createElement('div')
-            card.classList.add('review-card')
-            card.innerHTML = topTwo[i].stars + ' stars from ' + topTwo[i].name
-            reviewContainer.appendChild(card)
-        }
-        container.removeChild(button) 
+let count = 0;
+function addReviews(array: Review[]): void {
+  if (!count) {
+    count++;
+    const topTwo = getTopTwoReviews(array);
+    for (let i = 0; i < topTwo.length; i++) {
+      const card = document.createElement("div");
+      card.classList.add("review-card");
+      card.innerHTML = topTwo[i].stars + " stars from " + topTwo[i].name;
+      reviewContainer.appendChild(card);
     }
+    container.removeChild(button);
+  }
 }
 
-button.addEventListener('click', () => addReviews(reviews))
+button.addEventListener("click", () => addReviews(reviews));
 // location
-let currentLocation: [string, string, number] = ['Phokeng', '20:30', 12]
-footer.innerHTML = currentLocation[0] + ' ' + currentLocation[1] + ' ' + currentLocation[2] + '°'
- 
-
-
+let currentLocation: [string, string, number] = ["Phokeng", "20:30", 12];
+footer.innerHTML =
+  currentLocation[0] +
+  " " +
+  currentLocation[1] +
+  " " +
+  currentLocation[2] +
+  "°";
 
 let yourMainProperty = new MainProperty(
-  '../src/images/italian-property.jpg', 
-  'Italian House',
-  [{
-      name: 'Olive',
+  "../src/images/italian-property.jpg",
+  "Italian House",
+  [
+    {
+      name: "Olive",
       stars: 5,
       loyaltyUser: LoyaltyUser.GOLD_USER,
-      date: '12-04-2021'
-  }] )
+      date: "12-04-2021",
+    },
+  ]
+);
 
-const mainImageContainer = document.querySelector('.main-image') as HTMLElement
-const image = document.createElement('img')
-image.setAttribute('src', yourMainProperty.src)
-mainImageContainer.appendChild(image)
+const mainImageContainer = document.querySelector(".main-image") as HTMLElement;
+const image = document.createElement("img");
+image.setAttribute("src", yourMainProperty.src);
+mainImageContainer.appendChild(image);
